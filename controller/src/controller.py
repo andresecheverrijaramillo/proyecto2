32bb014648045ca04e1d3458dd76c098b5d08262
andresecheverrijaramillo <aecheverrj@eafit.edu.co>
2023-05-18 20:41:55 -0500

    registros
    registros y actulizado
    
    Co-Authored-By: jmfonsecap <68928395+jmfonsecap@users.noreply.github.com>
    Co-Authored-By: svelezp <69641241+svelezp@users.noreply.github.com>

--- controller/src/controller.py
+++ controller/src/controller.py
@@ -1,3 +1,4 @@
+import logging
 import boto3
 import boto3.session
 from concurrent import futures
@@ -10,26 +11,31 @@ import controller_pb2_grpc
 HOST = '[::]:8080'
 my_session = boto3.session.Session()
 
-
+archivo = open('archivo.txt', 'w')
 oldInstances=[]
 newInstances=[]
 
 resource_ec2 = boto3.client("ec2", 
-                            aws_access_key_id='ASIA5KF4RYNQ5REV6EXA',
-                            aws_secret_access_key='eYNElknn1U3ErrhnNtqNB9zitP7v8U2oRI6dutsJ',
-                            aws_session_token='FwoGZXIvYXdzEO3//////////wEaDDSTi8EZMptRjsmBXyLHAVWzJZIquyW7ajnaXCD8PkHizqdc90pUg8rekG5ZFDfSpDq+gVHP4YrxuH+tdkEsAxGKUUTaAWfsr7Qo7G6Y9wo0FjGkUw+Slw3cvutJfRSwrajIMOOWbUqoTLphJkniAuwQ7zWIVobfJf/y7YQOnt4SmtNmprBUAutK9Rh89awef95QKDtC0zHH1etSqTh61+1+fQTFXtRLXSamCnnl1MAYduEjEcJWtVTNigBQ0kRZ3nbkLnY28je0qXZ8cbrSwMkzJN0s+JUo3fiEowYyLZA1Cr+fhqd88qv9tr+9e13cLwMz75evyIvh8HcFLV3VFuP7D+V/gnM2oRso4Q==',
+                            aws_access_key_id="ASIATJHWQLIKHMRBX33N",
+                            aws_secret_access_key="Lxuhfw/1QbplOPLiGVdcV3mZysdm11iJdEksyDVV",
+                            aws_session_token="FwoGZXIvYXdzEDEaDDSofgEtCoULlhPnYCLIAXh0RZwJrFppEd0BgIb1DbdWAMyAEB+gSRuakWd3F7SU96QokZd81Tj+r1XUkL7AVQMKiLnFv8Zomx8tGyLV6s/8niMcCTOsXTBRvAjDA5c6jtZs91dL3XQkFJ1YR0aUSP0fIX28BNWRW1pxJcDTO/JL+0q5bhBR8KCd2JU1xBEyrI3+nXNZyMd1j9rfgsni5qROvapb9tROvF6GrQxruaS4s1/83iJcoLY9jI0IQXCMZQ68slJzMGAlf5OwRS4c6FKJqmFfJvBKKIbik6MGMi3714yu6tchfkRxGZA64fHfomou8lwyYqhADsbTofRZO5zXQ/e1O+lm9e60IBo=",
                             region_name='us-east-1')
+
+lt = {
+    'LaunchTemplateId': 'lt-0b369b2430457df2b',
+}
 
 def create_ec2_instance():
     get_old_instances()
     try:
         print ("Creating EC2 instance")
         resource_ec2.run_instances(
-            ImageId="ami-0242669d1b95f4db5",
+            LaunchTemplate=lt,
             MinCount=1,
             MaxCount=1,
             InstanceType="t2.micro",
-            KeyName="Andres")
+            KeyName="TeleKey")
+        time.sleep(1)
         get_new_instance()
         
     except Exception as e:
@@ -61,15 +67,14 @@ def get_new_instance():
 def get_ipv4(instance_id):
     response = resource_ec2.describe_instances(InstanceIds=[instance_id])
     ipv4_publico = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    print(f"La dirección IPv4 pública de la instancia {instance_id} es {ipv4_publico}")
     return ipv4_publico
 
 def terminate_ec2_instance(instance_id):
     try:
-        print ("Terminate EC2 instance")
-        print(resource_ec2.terminate_instances(InstanceIds=[instance_id]))
+        archivo.write("Terminate EC2 instance")
+        archivo.write(resource_ec2.terminate_instances(InstanceIds=[instance_id]))
         newInstances.remove(instance_id)
-        return "Instacia " + instance_id+ " terminada"
+        return "Instancia " + instance_id+ " terminada"
     except Exception as e:
         print(e)
         return 
@@ -85,20 +90,53 @@ def Ping(ipv4_publica):
         stub = controller_pb2_grpc.controllerStub(channel)
         response = stub.Ping(controller_pb2.Nada())
     return response
-            
-if __name__ == "_main_":
+def serve():
+    with open('archivo.txt', 'w') as archivo:
+        archivo.truncate()
     starttime = time.time()
+    minimum_instances()
+    print("waiting for instances to launch")
+    archivo.write("waiting for instances to launch")
+    time.sleep(180)
     while True:
         minimum_instances()
         for instance in newInstances:
             ip = get_ipv4(instance)
+            try:
+                response= Ping(ip)
+                if response.status_code==0:
+                    print(ip+" esta activa y la ocupacion de su cpu es de "+ str(response.cpu_usage))
+                    archivo.write(ip+" esta activa y la ocupacion de su cpu es de "+ str(response.cpu_usage))
+                    if response.cpu_usage>50 and response.cpu_usage<80:
+                        if len(newInstances)<4:
+                            create_ec2_instance()
+                    elif response.cpu_usage>80:
+                        terminate_ec2_instance(instance)
+            except:
+                print(ip+" esta prendiendo")
+                archivo.write(ip+" esta prendiendo")
+            
+        time.sleep(30.0-((time.time() - starttime)%30.0))
+
+def serve2():
+    starttime = time.time()
+    while True:
+        ip ="3.93.20.58"
+        try:
             response= Ping(ip)
             if response.status_code==0:
-                print(ip+" esta activa y la ocupacion de su cpu es de "+ response.cpu_usage)
-                if response.cpu_usage>50 and len(newInstances)<4 :
-                    create_ec2_instance()
+                print(ip+" esta activa y la ocupacion de su cpu es de "+ str(response.cpu_usage))
+                
+                if response.cpu_usage>50 and len(newInstances)<4 and response.cpu_usage<80:
+                    #create_ec2_instance()
+                    print("hola")
                 elif response.cpu_usage>80:
-                    terminate_ec2_instance(instance)
-            elif response.status_coded==1:
-                print(ip+" esta caida" )
-        time.sleep(30.0-((time.time() - starttime)%30.0))
+                    #terminate_ec2_instance(instance)
+                    print("hola")
+        except Exception as e:
+            print(e)
+        
+        time.sleep(5.0-((time.time() - starttime)%5.0))        
+if __name__ == '__main__':
+    logging.basicConfig()
+    serve()
